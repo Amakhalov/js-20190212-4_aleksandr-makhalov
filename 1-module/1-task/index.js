ce39a794898835d0adfdb99a800b011d4e67699a
Aleksandr Makhalov <amakhalov@gmail.com>
2019-04-01 17:30:58 +0300

    добавил проверку на натуральные числа, добавил функцию

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,6 +4,11 @@
  * @param {number} n index
  * @returns {number}
  */
-function pow (m, n) {
 
+let base = prompt ('put a num', '1');
+let indx = prompt ('put an indx', '1');
+
+function pow (m, n) {
+    if (n < 1 && m < 1 && (n % Math.floor(n) != 0) && (n % Math.floor(n) != 0)) return alert("put a natural num & indx");
+    for ( let i = 0; i<n; i++) m *= n;
 }
