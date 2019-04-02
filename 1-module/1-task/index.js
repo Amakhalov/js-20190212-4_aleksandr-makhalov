e1f51c4de850352c8f2bfb0c9391a86cd33c8a4e
Aleksandr Makhalov <amakhalov@gmail.com>
2019-04-02 17:49:11 +0300

    исправил формулу расчёта в цикле

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -11,7 +11,7 @@ let indx = prompt ('put an indx', '1');
 function pow (m, n) {
     // if (n < 1 && m < 1 && (n % Math.floor(n) != 0) && (n % Math.floor(n) != 0)) return alert("put a natural num & indx");
     if (n < 1 && m < 1) return;
-    for ( let i = 0; i<n; i++) m *= n;
+    for ( let i = 0; i<n; i++) m *= m;
     return m;
 };
  pow(base, indx);
