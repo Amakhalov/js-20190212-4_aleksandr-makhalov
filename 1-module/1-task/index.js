7db36d91dd397e822151f85a51c131d0cfb659f3
Aleksandr Makhalov <amakhalov@gmail.com>
2019-04-02 17:33:35 +0300

    испраил условия

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -9,8 +9,9 @@ let base = prompt ('put a num', '1');
 let indx = prompt ('put an indx', '1');
 
 function pow (m, n) {
-    if (n < 1 && m < 1 && (n % Math.floor(n) != 0) && (n % Math.floor(n) != 0)) return alert("put a natural num & indx");
+    // if (n < 1 && m < 1 && (n % Math.floor(n) != 0) && (n % Math.floor(n) != 0)) return alert("put a natural num & indx");
+    if (n < 1 && m < 1) return;
     for ( let i = 0; i<n; i++) m *= n;
-    alert(m);
+    return m;
 };
  pow(base, indx);
