d38618bce37d35396f63923d551581e8cc48f856
Aleksandr Makhalov <amakhalov@gmail.com>
2019-04-02 17:53:18 +0300

    исправил код в цикле

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -11,7 +11,8 @@ let indx = prompt ('put an indx', '1');
 function pow (m, n) {
     // if (n < 1 && m < 1 && (n % Math.floor(n) != 0) && (n % Math.floor(n) != 0)) return alert("put a natural num & indx");
     if (n < 1 && m < 1) return;
-    for ( let i = 0; i<n; i++) m *= m;
+    let tempBase = m;
+    for ( let i = 0; i<n; i++) m *= tempBase;
     return m;
 };
  pow(base, indx);
