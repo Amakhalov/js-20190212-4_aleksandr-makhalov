24792d430cc0ecb7b1faff24f4eb5490bebfbad5
Aleksandr Makhalov <amakhalov@gmail.com>
2019-04-03 09:38:39 +0300

    добавил условия

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -9,10 +9,11 @@ let base = prompt ('put a num', '1');
 let indx = prompt ('put an indx', '1');
 
 function pow (m, n) {
-    // if (n < 1 && m < 1 && (n % Math.floor(n) != 0) && (n % Math.floor(n) != 0)) return alert("put a natural num & indx");
-    if (n < 1 && m < 1) return;
+    if (n < 1 && m < 1 && (n % Math.floor(n) != 0) && (n % Math.floor(n) != 0)) return;
+    //if (n < 1 && m < 1) return;
     let tempBase = m;
-    for ( let i = 0; i<n; i++) m *= tempBase;
+    for ( let i = 1; i<n; i++) m *= tempBase;
+    console.log(m);
     return m;
 };
  pow(base, indx);
