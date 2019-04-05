0032bf41400e8312bef06bdadd39e6c74de104c9
Aleksandr Makhalov <amakhalov@gmail.com>
2019-04-05 14:53:30 +0300

    написал функцию клонирования своств через for (key in obj)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,19 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+
 function clone (obj) {
+    let objClone = {};
+    for (let key in obj) {
+        objClone[key] = obj[key];
+    }
+    return objClone
 }
+
+// let obj = {test: 1};
+// let obj = {test: { test2: { test3: '1' } }};
+// let obj = {test: { test2: { test3: null } }};
+
+// console.log(obj);
+// console.log(clone(obj));
+// console.log(clone(obj).test.test2.test3 === obj.test.test2.test3);
