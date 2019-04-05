68bcf56e9227fc3eeb9f67aa6276d5e2fc5443f4
Aleksandr Makhalov <amakhalov@gmail.com>
2019-04-05 12:25:32 +0300

    исправил, теперь есть split по ','

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -8,8 +8,21 @@
 
 
 function getMinMax(str) {
-    const arr = str.split(' ');
-    const arrNum = arr.map(function(item) {
+    let arr = str.split(' ');
+
+    console.log(arr);
+
+    let arr2 = [];
+    arr.forEach(function(item){
+        let localArr = item.split(',');
+        localArr.forEach(function(item, i) {
+            arr2.push(localArr[i]);
+        });
+    });
+
+    console.log(arr2);
+
+    const arrNum = arr2.map(function(item) {
         return parseFloat(item);
     });
     let arrNum2 = arrNum.filter(function(item) {
@@ -22,12 +35,8 @@ function getMinMax(str) {
         min: arrNum2[0],
         max: arrNum2[arrNum2.length - 1]
     };
-    // arrNum3.push(arrNum2[0]);
-    // arrNum3.push(arrNum2[arrNum2.length - 1]);
     return result;
 }
-
-// var inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
-
+// var inputData = '1,-111 -5.8 или 10, хотя 34 + -5.3 и 73';
 // console.log(getMinMax(inputData));
 // console.log(parseFloat('-5.8'));
