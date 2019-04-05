7588e2cca3cf604ffe9b0971306beed3f51b41b7
Aleksandr Makhalov <amakhalov@gmail.com>
2019-04-05 11:47:00 +0300

    написал функцию

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,26 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
 
+
+function getMinMax(str) {
+    const arr = str.split(' ');
+    const arrNum = arr.map(function(item) {
+        return parseFloat(item);
+    });
+    let arrNum2 = arrNum.filter(function(item) {
+        return +item;
+    });
+    arrNum2 = arrNum2.sort(function(a, b) {
+        return a - b;
+      });
+    let arrNum3 = [];
+    arrNum3.push(arrNum2[0]);
+    arrNum3.push(arrNum2[arrNum2.length - 1]);
+    return arrNum3;
 }
 
+// var inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
+
+// console.log(getMinMax(inputData));
+// console.log(parseFloat('-5.8'));
